19911b1979df2eb0ee56ac90de499e9657a8022c
arpitabhi <arpitabhi@gmail.com>
2020-11-28 02:16:33 +0530

    added code for booking and cancellation

--- booking.py
+++ booking.py
@@ -1,6 +1,50 @@
 
-global bookings
+class MR_booking:
 
-bookings = {}
 
-def 
+    global bookings_list
+    global number_of_available_rooms
+
+    bookings_list = []
+    number_of_available_rooms = 100
+
+    def booking_func(employee_id,number_of_rooms,start_time,end_time,number_of_employees):
+        if len(bookings_list) == 0:
+            booking_id = 1
+        else:
+            booking_id = bookings_list[-1]['booking_id'] + 1
+        
+        booking_elements = {}
+        booking_elements['booking_id'] = booking_id
+        booking_elements['employee_id'] = employee_id
+        booking_elements['number_of_rooms'] = number_of_rooms
+        booking_elements['start_time'] = start_time
+        booking_elements['end_time'] = end_time
+        booking_elements['number_of_employees'] = number_of_employees
+
+        bookings_list.append(booking_elements)
+
+        return bookings_list
+    
+    def cancellation_func(booking_id):
+        flag = False
+        index = None
+        for i in bookings_list:
+            if i['booking_id'] == booking_id:
+                flag = True
+                index = i
+                break
+        if flag:
+            bookings_list.pop(index)
+        else:
+            print('booking_id not found')
+        return True
+
+    
+
+        
+        
+        
+            
+
+
